539c8f909bfa9d90c1f94c0cde75f3fd79cfb24d
Isabel Amaral <isabel.andre.amaral@gmail.com>
2026-07-08 11:35:51 +0100

    Implemented template4 repair

--- repair/Templates/Template4.cs
+++ repair/Templates/Template4.cs
@@ -7,6 +7,23 @@ public class Template4(int snapTargetPos, string snapTargetPred, bool snapTarget
     : StateChangingAssignTemplate(snapTargetPos, snapTargetPred, stateChangingTargetAssignVar, stateChangingTargetAssignType, reporter)
 {
     protected override void InstantiateTemplate() {
-        throw new NotImplementedException();
+        if (SuspiciousStmt == null || SuspiciousBlockStmt == null || SnapTargetPred == null)
+            return;
+
+        var assign = CreateStateChangingAssignment();
+        var faultyStmtIdx = SuspiciousBlockStmt.Body.IndexOf(SuspiciousStmt);
+        if (assign == null || faultyStmtIdx == -1) 
+            return;
+        SuspiciousBlockStmt.Body.RemoveAt(faultyStmtIdx);
+        
+        var snapTargetValLiteral = new LiteralExpr(null, snapTargetVal);
+        var ifSnapPredStmtGuard = new BinaryExpr(null, 
+            BinaryExpr.Opcode.Eq, SnapTargetPred, snapTargetValLiteral);
+        var ifSnapPredStmtThnBody = new BlockStmt(null, [assign]);
+        var ifSnapPredStmtElsBody = new BlockStmt(null, [SuspiciousStmt]);
+        var ifSnapPredStmt = new IfStmt(null, 
+            false, ifSnapPredStmtGuard, 
+            ifSnapPredStmtThnBody, ifSnapPredStmtElsBody);
+        SuspiciousBlockStmt.Body.Insert(faultyStmtIdx, ifSnapPredStmt);
     }
 }
