58c1ae2c94b2e5cb854025db66fdb254983c37e2
Isabel Amaral <isabel.andre.amaral@gmail.com>
2026-07-08 11:24:34 +0100

    Implemented template3 repair

--- repair/Templates/Template3.cs
+++ repair/Templates/Template3.cs
@@ -6,6 +6,23 @@ public class Template3(int snapTargetPos, string snapTargetPred, bool snapTarget
     : Template(snapTargetPos, snapTargetPred, reporter)
 {
     protected override void InstantiateTemplate() {
-        throw new NotImplementedException();
+        if (SuspiciousStmt == null || SuspiciousBlockStmt == null)
+            return;
+        
+        var faultyStmtIdx = SuspiciousBlockStmt.Body.IndexOf(SuspiciousStmt);
+        if (faultyStmtIdx == -1) 
+            return;
+        SuspiciousBlockStmt.Body.RemoveAt(faultyStmtIdx);
+        
+        var snapTargetValLiteral = new LiteralExpr(null, snapTargetVal);
+        var ifSnapPredStmtGuard = new BinaryExpr(null, 
+            BinaryExpr.Opcode.Eq, SnapTargetPred, snapTargetValLiteral);
+        var ifSnapPredStmtGuardNeg = new UnaryOpExpr(null, 
+            UnaryOpExpr.Opcode.Not, ifSnapPredStmtGuard);
+        var ifSnapPredStmtBody = new BlockStmt(null, [SuspiciousStmt]);
+        var ifSnapPredStmt = new IfStmt(null, 
+            false, ifSnapPredStmtGuardNeg, 
+            ifSnapPredStmtBody, null);
+        SuspiciousBlockStmt.Body.Insert(faultyStmtIdx, ifSnapPredStmt);
     }
 }
